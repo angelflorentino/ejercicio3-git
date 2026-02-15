9242f07cfe1a5d01ebf1d4eea90c06fb63a0c499
Angel Florentino <angel.floren@hotmail.com>
2026-02-15 13:32:13 -0600

    Primer commit. Agregado el # cuenta y el hola mundo dentro de program

--- Ejercicio 3 - Git/Program.cs
+++ Ejercicio 3 - Git/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Angel Gustavo Florentino Donaire
+// Cuenta: 20241900213
+
+    ﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Console.WriteLine("Hola mundo!");
+
+        }
+    }
